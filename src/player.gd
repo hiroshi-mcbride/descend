e6abc35626549a7e174a9c0376485b34161f07d3
Michael Yamada <roninnarasu@gmail.com>
2024-01-12 16:16:50 +0100

    camera look improvements

--- src/player.gd
+++ src/player.gd
@@ -2,6 +2,10 @@ extends CharacterBody3D
 
 @onready var head : Node3D = get_node("feller2/head")
 var look_vector : Vector2
+@export var mouse_sensitivity : float
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta):
 	var inputVector = Input.get_vector("move_left", "move_right", "move_down", "move_up")
@@ -11,8 +15,8 @@ func _physics_process(delta):
 	var rot_x = head.rotation.x - look_vector.y * delta
 	rot_x = clampf(rot_x, -PI*0.4, PI*0.49)
 	rot_x -= head.rotation.x
-	head.rotate_object_local(Vector3(1,0,0), rot_x)
-	rotate_object_local(Vector3(0,1,0),-look_vector.x * delta)
+	head.rotate_object_local(Vector3(1,0,0), rot_x * mouse_sensitivity)
+	rotate_object_local(Vector3(0,1,0),-look_vector.x * mouse_sensitivity * delta)
 	look_vector = Vector2.ZERO
 
 func _unhandled_input(event):
